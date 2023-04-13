9f7d55902997432b877fb0aae4b0504fcfd9ba76
Alexander <72903349+alavertu36@users.noreply.github.com>
2023-04-13 15:08:19 -0600

    My Second Project Week 13

---
+++ MySecondProject/App copy 2.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function MyApp() {
+
+  function getFullName(fName, mName, lName) {
+    return fName + " " + mName + " " + lName;
+  }
+  const pet = "Dog";
+  return (
+    <Text>
+      {"\n\n\n\n\n\n"}
+      Hello I am a student in CIS340! {"\n"}
+      My full name is {getFullName("Alexander", "Biron", "Lavertu")} {"\n"}
+      I have a {pet}!
+      </Text>
+  );
+}
