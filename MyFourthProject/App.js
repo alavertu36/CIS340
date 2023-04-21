72cd1832211c22adc165802b9d459c3ebe0e496e
Alexander <72903349+alavertu36@users.noreply.github.com>
2023-04-21 11:22:07 -0600

    updated project 4
    Listview and sectionlist

--- MyFourthProject/App.js
+++ MyFourthProject/App.js
@@ -1,56 +1,27 @@
 import React from 'react';
-import { Text, Image, ScrollView } from 'react-native';
+import { Text, View, SectionList } from 'react-native';
 
-const dog = {
-  uri: 'https://raw.githubusercontent.com/AbdunabiRamadan/CIS340/master/images/dog2.png',
-  width: 64,
-  height: 64
-};
-
-export default MyScrollViewApp = () => {
-
-
-    <ScrollView style={{padding: 40}}>
-      <Text style={{fontSize: 80}}> Try to scroll down </Text>
-      <Image source={require('./assets/dog2.png')} style={{width:80, height: 80}} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Text style={{fontSize: 80}}> Try to scroll down again, if you like</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Text style={{fontSize: 80}}> Try to scroll down</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Text style={{fontSize: 80}}> Try to scroll down</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Text style={{fontSize: 80}}> React Native Components!</Text>
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-      <Image source={dog} />
-    </ScrollView>
-};
+export default StatesApp = () => {
+   return (
+    <View style={{flex: 1, paddingTop: 22}}>
+    <SectionList
+      sections={[
+        {title: 'A', data: ['Alabama', 'Alaska', 'Arizona', 'Arkansas']},
+        {title: 'C', data: ['California', 'Colorado', 'Connecticut']},
+        {title: 'D', data: ['Delaware']},
+        {title: 'F', data: ['Florida']},
+        {title: 'G', data: ['Georgia']},
+        {title: 'H', data: ['Hawaii']},
+      ]}
+      renderItem={({item}) => <Text style={{padding: 10, fontSize: 20, height: 44,}}> {item} </Text>}
+      renderSectionHeader={({section}) => <Text style={{paddingTop: 4, paddingLeft: 10, paddingRight: 10,
+      paddingBottom: 4,
+      fontSize: 14,
+      fontWeight: 'bold',
+      backgroundColor: '#9FA8DF',}}>{section.title}</Text>}
+      keyExtractor={(item, index) => index}
+      />
+      </View>
+   );
+}
 
